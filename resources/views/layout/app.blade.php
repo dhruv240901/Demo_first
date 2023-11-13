7f7201cf4633258e7a9bd5866af714f213fdf8e4
Dhruv531142 <65854604+Dhruv531142@users.noreply.github.com>
2023-11-13 22:37:23 +0530

    change title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Student</title>
 
         @include('include.css')
         @include('include.js')
